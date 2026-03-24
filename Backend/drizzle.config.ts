94e91e26f17193ee8d62381aa852bcca751b9dfe
Aakash1220 <chaturvediaakash98@gmail.com>
2026-03-24 10:52:39 +0100

    backend setup completion

---
+++ Backend/drizzle.config.ts
@@ -0,0 +1,9 @@
+import {defineConfig} from 'drizzle-kit'
+import {ENV} from './src/Configs/env'
+
+export default defineConfig({
+schema:'./src/db/schema.ts',
+dialect:'postgresql',
+dbCredentials:{url:ENV.DB_URL!},
+
+})
--- Backend/src/db/indext.ts
+++ Backend/src/db/indext.ts
@@ -2,3 +2,18 @@ import { drizzle } from 'drizzle-orm/node-postgres';
 import * as schema from './schema';
 import { ENV } from "../Configs/env";
 import { Pool } from 'pg';
+
+if(!ENV.DB_URL){
+    throw new Error('Database URL not found');
+}
+let pool = new Pool({connectionString:ENV.DB_URL})
+pool.on('connect',()=>{
+    console.log('Database connected successfully');
+    
+})
+pool.on('error',()=>{
+    console.log('Database not connected');
+    
+})
+
+export const db = drizzle({client:pool,schema})
